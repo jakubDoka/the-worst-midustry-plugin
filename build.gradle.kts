7b3fbf4b934a47092552b84eda5d476860f00f76
Jakub Dóka <60517552+jakubDoka@users.noreply.github.com>
2021-05-10 16:12:26 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://www.jitpack.io") }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    compileOnly("com.github.Anuken.Arc:arc-core:v104.6")
+    compileOnly("com.github.Anuken.Mindustry:core:v104.6")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    jar {
+        from(configurations.compileClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    }
+}
